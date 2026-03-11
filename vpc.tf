15a694d4358ccc0f8e2898a70d2fbfff6be4c4b1
Aryakadam <kadam.arya9058@gmail.com>
2026-03-11 16:48:15 +0530

    rds database add

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,8 @@ provider "aws" {
 
 resource "aws_vpc" "my-vpc" {
   cidr_block = var.mumbai_vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name= var.vpc_name
   }
