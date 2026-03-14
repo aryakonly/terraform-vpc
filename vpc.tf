fb37e67ed2609bbb33d3f96361e1e4cefb94dc66
Aryakadam <kadam.arya9058@gmail.com>
2026-03-14 20:12:16 +0530

    userdata.sh is added

--- vpc.tf
+++ vpc.tf
@@ -202,27 +202,7 @@ resource "aws_instance" "db-instance" {
     key_name = var.instance_key
     vpc_security_group_ids = [aws_security_group.my-sg-1.id]
     subnet_id = aws_subnet.mysubnet-1.id
-    user_data = <<-EOF
-    #!/bin/bash
-    yum install update -y
-    yum install mariadb-server -y
-    systemctl start mariadb.service
-    systemctl enable mariadb.service
-
-    mysql -h ${aws_db_instance.my_db.address} -u arya -pAryakadam47 <<MYSQL
-    create database studentapp;
-    use studentapp;
-    CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
-	    student_name VARCHAR(100) NOT NULL,
-      student_addr VARCHAR(100) NOT NULL,
-  	  student_age VARCHAR(3) NOT NULL,
-	    student_qual VARCHAR(20) NOT NULL,
-	    student_percent VARCHAR(10) NOT NULL,
-  	  student_year_passed VARCHAR(10) NOT NULL,
-	    PRIMARY KEY (student_id)
-	  );
-MYSQL
-    EOF
+    user_data = base64decode(file("userdata.sh"))
     tags = {
       Name = var.private_instance_name
     }
